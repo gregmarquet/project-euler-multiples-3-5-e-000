acaf2cd563a54546116022afd03a1eb50877334e
gregmarquet <marquet.greg@gmail.com>
2016-11-19 11:29:18 -0500

    Done

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,10 @@
 # Enter your procedural solution here!
+
+def collect_multiples(num)
+  (1...num).select { |num|  num % 3 == 0 || num % 5 ==0 }
+end
+
+def sum_multiples(num)
+  collect_multiples(num).inject(0, :+)
+end
+
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,16 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+  def initialize(num)
+    @num = num
+  end
+
+  def collect_multiples
+    (1...@num).select { |num|  num % 3 == 0 || num % 5 ==0 }
+  end
+
+  def sum_multiples
+    collect_multiples.inject(0, :+)
+  end
+
+end
